97c80e676854dc9b4940a6f4bc78a5a8811e535c
Frederick Blais <fred_blais5@hotmail.com>
2014-08-12 17:06:13 -0400

    flyout test

--- default.js
+++ default.js
@@ -5,6 +5,15 @@ WinJS.Application.onready = function () {
     // The next line will apply declarative control binding to all elements
     // (e.g. DIV with attribute: data-win-control="WinJS.UI.Rating")
     WinJS.UI.processAll();
+    var button1 = document.getElementById("btn1");
+    button1.addEventListener("click", button1Click, false);
 };
 
 WinJS.Application.start();
+
+function button1Click() {
+    console.log("click")
+    //alert("allo mek", "titre");
+    var button1 = document.getElementById("btn1");
+    document.getElementById("confirmFlyout").winControl.show(button1);
+}
