7e888641313266a56a5e7f45a09bf87fe0129b8f
= <fred_blais5@hotmail.com>
2014-08-14 12:20:21 -0400

    Display task in output div

--- default.js
+++ default.js
@@ -7,14 +7,29 @@ WinJS.Application.onready = function () {
     // (e.g. DIV with attribute: data-win-control="WinJS.UI.Rating")
     WinJS.UI.processAll();
 	var addTaskBtn = document.getElementById("addTask");
-    addTaskBtn.addEventListener("click", AddTask, false);
-	alert("end");
-	
+	var createTaskBtn = document.getElementById("createTask");
+    addTaskBtn.addEventListener("click", ShowAddTaskFlyout, false);
+	createTaskBtn.addEventListener("click", DisplayTask, false);	
 };
 
 WinJS.Application.start();
 
-function AddTask() {
-	alert("flyout");
+
+
+function ShowAddTaskFlyout() {
+	var addTaskBtn = document.getElementById("addTask");
     document.getElementById("addTaskFlyout").winControl.show(addTaskBtn, "right");
+}
+
+function DisplayTask() {
+	var createTaskBtn = document.getElementById("createTask");
+	var taskListDiv = document.getElementById("taskList");
+	var taskNameInput = document.getElementById("taskName");
+	
+	var taskName = taskNameInput.value;
+	var task = document.createElement("p");
+	var textNode = document.createTextNode(taskName);
+	task.appendChild(textNode);
+	taskListDiv.appendChild(task);
+
 }
