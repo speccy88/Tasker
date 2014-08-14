693f084fdb47a9379a82b7c9f274a1986dc72c44
= <fred_blais5@hotmail.com>
2014-08-13 23:38:38 -0400

    added stuff

--- default.js
+++ default.js
@@ -1,19 +1,20 @@
 // Add your TypeScript or JavaScript here 
 
+
 WinJS.Application.onready = function () {
 
     // The next line will apply declarative control binding to all elements
     // (e.g. DIV with attribute: data-win-control="WinJS.UI.Rating")
     WinJS.UI.processAll();
-    var button1 = document.getElementById("btn1");
-    button1.addEventListener("click", button1Click, false);
+	var addTaskBtn = document.getElementById("addTask");
+    addTaskBtn.addEventListener("click", AddTask, false);
+	alert("end");
+	
 };
 
 WinJS.Application.start();
 
-function button1Click() {
-    console.log("click")
-    //alert("allo mek", "titre");
-    var button1 = document.getElementById("btn1");
-    document.getElementById("confirmFlyout").winControl.show(button1);
+function AddTask() {
+	alert("flyout");
+    document.getElementById("addTaskFlyout").winControl.show(addTaskBtn, "right");
 }
